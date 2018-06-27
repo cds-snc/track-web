67247946d699f494d6ad6c4e2455441c2bdb05d4
David Buckley <buckley.w.david@gmail.com>
2018-06-27 17:31:49 -0400

    add dockerbuild for building and packaging environment

---
+++ build-env.sh
@@ -0,0 +1,9 @@
+WORKDIR=${1:-"/home/DEV.T1.TBS-SCT.GC.CA/dsamojle-ps/"}
+mkdir -p $WORKDIR
+cd $WORKDIR
+python3 -m venv .venv
+. .venv/bin/activate
+pip install wheel
+pip install -r requirements.txt
+tar -czvf track-web.tar.gz .venv track
+rm -rf .venv
