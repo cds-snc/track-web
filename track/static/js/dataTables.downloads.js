8bb0804e9f19c4480c19357afdd181926c62761a
evadb <eva.demers-brett@cds-snc.ca>
2018-06-29 15:29:09 -0400

    Add gtag events back in (#49)

--- track/static/js/dataTables.downloads.js
+++ track/static/js/dataTables.downloads.js
@@ -36,7 +36,7 @@ $.fn.dataTable.Download = function ( inst ) {
     if (drawnOnce) return;
 
     var elem = "" +
-      "<a class=\"text-https-blue hover:text-black font-bold\" href=\"" + csv + "\" download>" +
+      "<a onClick=\"gtag('event', 'download', { event_category: 'Downloads', event_action: 'Download full CSV'});\" class=\"text-https-blue hover:text-black font-bold\" href=\"" + csv + "\" download>" +
         text +
       "</a>";
 
