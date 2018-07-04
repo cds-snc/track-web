cae53488c768d8f3f6cdc188904fe11573cbca33
David Buckley <buckley.w.david@gmail.com>
2018-07-04 14:59:22 -0400

    add docker-compose for easy building

--- deploy/build-env.sh
+++ deploy/build-env.sh
@@ -3,7 +3,7 @@ mkdir -p $WORKDIR
 cd $WORKDIR
 python3 -m venv .venv
 . .venv/bin/activate
-pip install wheel
+pip install --upgrade pip
 pip install -r requirements.txt
 tar -czvf track-web.tar.gz .venv track
 rm -rf .venv
