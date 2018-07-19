9f1669d9d76150e6ffdd3b320dc8a6cfac70b682
David Buckley <buckley.w.david@gmail.com>
2018-07-18 23:25:49 -0400

    Add support for a flag to drop the cache

--- track/models.py
+++ track/models.py
@@ -281,3 +281,19 @@ class Organization:
     @staticmethod
     def all() -> typing.Iterable[typing.Dict]:
         return db.db.meta.find({'_collection': 'organizations'}, {'_id': False, '_collection': False})
+
+
+class Flag:
+
+    @staticmethod
+    def get_cache() -> bool:
+        flags = db.db.meta.find_one({"_collection": "flags"})
+        if flags:
+            return flags['cache']
+        else:
+            Flag.set_cache(True)
+
+    @staticmethod
+    def set_cache(state: bool) -> None:
+        db.db.meta.update_one({"_collection": "flags"}, {"$set": {"cache": state}}, upsert=True)
+
