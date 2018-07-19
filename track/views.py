cff602f9728dfa0ce06352116f8e870de5a0d94f
Damien Trudel <39952986+damcom-gc@users.noreply.github.com>
2018-07-19 16:46:32 -0400

    Adding response headers and subresource integrity for #76 (#77)
    * Added three web response headers - X-Frame-Options, X-XSS-Protection, X-Content-Type-Options

--- track/views.py
+++ track/views.py
@@ -9,6 +9,7 @@ from track.data import FIELD_MAPPING
 from track import models
 from track.cache import cache
 
+
 def register(app):
 
     # Default route checks accept-language header
@@ -16,14 +17,14 @@ def register(app):
     @app.route("/")
     def index():
         user_lang = request.headers.get("Accept-Language", "en")
-        if (user_lang[:2] == "fr"):
+        if user_lang[:2] == "fr":
             return redirect("/fr/index/")
         else:
             return redirect("/en/index/")
 
     @app.route("/ping")
     def health_check():
-        return 'PONG'
+        return "PONG"
 
     @app.route("/en/")
     @app.route("/fr/")
@@ -58,8 +59,8 @@ def register(app):
         return render_template(generate_path(prefix, "help"))
 
     def generate_path(prefix, page_id):
-        if(prefix == 'en' or prefix == 'fr'):
-            return os.path.join(prefix, '{}.html'.format(page_id))
+        if prefix == "en" or prefix == "fr":
+            return os.path.join(prefix, "{}.html".format(page_id))
         else:
             abort(HTTPStatus.NOT_FOUND)
 
@@ -67,7 +68,7 @@ def register(app):
     def cache_bust():
         try:
             cache.clear()
-            return '', HTTPStatus.NO_CONTENT
+            return "", HTTPStatus.NO_CONTENT
         except Exception as exc:
             return str(exc), HTTPStatus.INTERNAL_SERVER_ERROR
 
@@ -78,175 +79,178 @@ def register(app):
     @app.route("/data/reports/<report_name>.json")
     @cache.cached()
     def report(report_name):
-        report_name = 'https' if report_name == 'compliance' else report_name
+        report_name = "https" if report_name == "compliance" else report_name
 
         response = Response(ujson.dumps(models.Report.latest().get(report_name, {})))
-        response.headers['Content-Type'] = 'application/json'
+        response.headers["Content-Type"] = "application/json"
         return response
 
     # Detailed data per-parent-domain.
     @app.route("/data/domains/<report_name>.json")
     @cache.cached()
     def domain_report(report_name):
-        report_name = 'https' if report_name == 'compliance' else report_name
+        report_name = "https" if report_name == "compliance" else report_name
 
         domains = models.Domain.eligible_parents(report_name)
-        domains = sorted(domains, key=lambda k: k['domain'])
+        domains = sorted(domains, key=lambda k: k["domain"])
 
-        response = Response(ujson.dumps({'data': domains}))
-        response.headers['Content-Type'] = 'application/json'
+        response = Response(ujson.dumps({"data": domains}))
+        response.headers["Content-Type"] = "application/json"
         return response
 
     @app.route("/data/domains/<language>/<report_name>.csv")
     @cache.cached()
     def domain_report_csv(report_name, language):
-        report_name = 'https' if report_name == 'compliance' else report_name
+        report_name = "https" if report_name == "compliance" else report_name
 
         domains = models.Domain.eligible_parents(report_name)
-        domains = sorted(domains, key=lambda k: k['domain'])
+        domains = sorted(domains, key=lambda k: k["domain"])
 
         response = Response(models.Domain.to_csv(domains, report_name, language))
-        response.headers['Content-Type'] = 'text/csv'
+        response.headers["Content-Type"] = "text/csv"
 
     @app.route("/data/domains-table.json")
     def domains_table():
         domains = models.Domain.find_all(
-            {'https.eligible_zone': True, 'is_parent': True},
+            {"https.eligible_zone": True, "is_parent": True},
             {
-                '_id': False,
-                'domain': True,
-                'organization_name_en': True,
-                'organization_name_fr': True,
-                'is_parent': True,
-                'base_domain': True,
-                'https.bod_crypto': True,
-                'https.eligible': True,
-                'https.enforces': True,
-                'https.hsts': True,
-                'https.compliant': True,
-                'https.bod_crypto': True,
-                'https.good_cert': True,
-                'totals.https.enforces': True,
-                'totals.https.hsts': True,
-                'totals.https.compliant': True,
-                'totals.https.eligible': True,
-                'totals.crypto.bod_crypto': True,
-                'totals.crypto.good_cert': True,
-                'totals.crypto.eligible': True,
-            }
+                "_id": False,
+                "domain": True,
+                "organization_name_en": True,
+                "organization_name_fr": True,
+                "is_parent": True,
+                "base_domain": True,
+                "https.bod_crypto": True,
+                "https.eligible": True,
+                "https.enforces": True,
+                "https.hsts": True,
+                "https.compliant": True,
+                "https.bod_crypto": True,
+                "https.good_cert": True,
+                "totals.https.enforces": True,
+                "totals.https.hsts": True,
+                "totals.https.compliant": True,
+                "totals.https.eligible": True,
+                "totals.crypto.bod_crypto": True,
+                "totals.crypto.good_cert": True,
+                "totals.crypto.eligible": True,
+            },
         )
-        response = Response(ujson.dumps({'data': domains}))
-        response.headers['Content-Type'] = 'application/json'
+        response = Response(ujson.dumps({"data": domains}))
+        response.headers["Content-Type"] = "application/json"
         return response
 
     @app.route("/data/organizations-table.json")
     def organizations_table():
         organizations = models.Organization.find_all(
-            {'https.eligible': {"$gt": 0}},
+            {"https.eligible": {"$gt": 0}},
             {
-                '_id': False,
-                'total_domains': True,
-                'name_en': True,
-                'name_fr': True,
-                'https.compliant': True,
-                'https.enforces': True,
-                'https.hsts': True,
-                'https.eligible': True,
-                'crypto.bod_crypto': True,
-                'crypto.good_cert': True,
-                'crypto.eligible': True,
-            }
+                "_id": False,
+                "total_domains": True,
+                "name_en": True,
+                "name_fr": True,
+                "https.compliant": True,
+                "https.enforces": True,
+                "https.hsts": True,
+                "https.eligible": True,
+                "crypto.bod_crypto": True,
+                "crypto.good_cert": True,
+                "crypto.eligible": True,
+            },
         )
         # app.logger.debug([o for o in organizations])
-        response = Response(ujson.dumps({'data': organizations}))
-        response.headers['Content-Type'] = 'application/json'
+        response = Response(ujson.dumps({"data": organizations}))
+        response.headers["Content-Type"] = "application/json"
         return response
-
 
     # Detailed data per-host for a given report.
     @app.route("/data/hosts/<report_name>.json")
     @cache.cached()
     def hostname_report(report_name):
-        report_name = 'https' if report_name == 'compliance' else report_name
+        report_name = "https" if report_name == "compliance" else report_name
 
         domains = models.Domain.eligible(report_name)
 
         # sort by base domain, but subdomain within them
-        domains = sorted(domains, key=lambda k: k['domain'])
-        domains = sorted(domains, key=lambda k: k['base_domain'])
+        domains = sorted(domains, key=lambda k: k["domain"])
+        domains = sorted(domains, key=lambda k: k["base_domain"])
 
-        response = Response(ujson.dumps({'data': domains}))
-        response.headers['Content-Type'] = 'application/json'
+        response = Response(ujson.dumps({"data": domains}))
+        response.headers["Content-Type"] = "application/json"
         return response
-
 
     @app.route("/data/hosts/<language>/<report_name>.csv")
     @cache.cached()
     def hostname_report_csv(language, report_name):
-        report_name = 'https' if report_name == 'compliance' else report_name
+        report_name = "https" if report_name == "compliance" else report_name
 
         domains = models.Domain.eligible(report_name)
 
         # sort by base domain, but subdomain within them
-        domains = sorted(domains, key=lambda k: k['domain'])
-        domains = sorted(domains, key=lambda k: k['base_domain'])
+        domains = sorted(domains, key=lambda k: k["domain"])
+        domains = sorted(domains, key=lambda k: k["base_domain"])
 
         response = Response(models.Domain.to_csv(domains, report_name, language))
-        response.headers['Content-Type'] = 'text/csv'
+        response.headers["Content-Type"] = "text/csv"
         return response
-
 
     # Detailed data for all subdomains of a given parent domain, for a given report.
     @app.route("/data/hosts/<domain>/<report_name>.json")
     @cache.cached()
     def hostname_report_for_domain(domain, report_name):
-        report_name = 'https' if report_name == 'compliance' else report_name
+        report_name = "https" if report_name == "compliance" else report_name
 
         domains = models.Domain.eligible_for_domain(domain, report_name)
 
         # sort by hostname, but put the parent at the top if it exist
-        domains = sorted(domains, key=lambda k: k['domain'])
-        domains = sorted(domains, key=lambda k: k['is_parent'], reverse=True)
+        domains = sorted(domains, key=lambda k: k["domain"])
+        domains = sorted(domains, key=lambda k: k["is_parent"], reverse=True)
 
-        response = Response(ujson.dumps({'data': domains}))
-        response.headers['Content-Type'] = 'application/json'
+        response = Response(ujson.dumps({"data": domains}))
+        response.headers["Content-Type"] = "application/json"
         return response
 
     # Detailed data for all subdomains of a given parent domain, for a given report.
     @app.route("/data/hosts/<domain>/<language>/<report_name>.csv")
     @cache.cached()
     def hostname_report_for_domain_csv(domain, language, report_name):
-        report_name = 'https' if report_name == 'compliance' else report_name
+        report_name = "https" if report_name == "compliance" else report_name
 
         domains = models.Domain.eligible_for_domain(domain, report_name)
 
         # sort by hostname, but put the parent at the top if it exist
-        domains = sorted(domains, key=lambda k: k['domain'])
-        domains = sorted(domains, key=lambda k: k['is_parent'], reverse=True)
+        domains = sorted(domains, key=lambda k: k["domain"])
+        domains = sorted(domains, key=lambda k: k["is_parent"], reverse=True)
 
         response = Response(models.Domain.to_csv(domains, report_name, language))
-        response.headers['Content-Type'] = 'text/csv'
+        response.headers["Content-Type"] = "text/csv"
         return response
-
 
     @app.route("/data/organizations/<report_name>.json")
     @cache.cached()
     def organization_report(report_name):
-        report_name = 'https' if report_name == 'compliance' else report_name
+        report_name = "https" if report_name == "compliance" else report_name
 
         domains = models.Organization.eligible(report_name)
-        response = Response(ujson.dumps({'data': domains}))
-        response.headers['Content-Type'] = 'application/json'
+        response = Response(ujson.dumps({"data": domains}))
+        response.headers["Content-Type"] = "application/json"
+        return response
+
+    # Every response back to the browser will include these web response headers
+    @app.after_request
+    def apply_headers(response):
+        response.headers["X-Frame-Options"] = "SAMEORIGIN"
+        response.headers["X-XSS-Protection"] = 1
+        response.headers["X-Content-Type-Options"] = "nosniff"
         return response
 
     @app.errorhandler(404)
     def page_not_found(error):
-        return render_template('404.html'), HTTPStatus.NOT_FOUND
+        return render_template("404.html"), HTTPStatus.NOT_FOUND
 
     @app.errorhandler(models.QueryError)
     def handle_invalid_usage(error):
         app.logger.error(error)
-        return render_template('404.html'), HTTPStatus.NOT_FOUND
 
     @app.before_request
     def verify_cache():
