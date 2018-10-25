b4ec42177c646cdc9ad243aa385bf22ba3b9a6c9
saya <sayaveth@gmail.com>
2018-10-25 15:52:11 -0400

    - removed temporary Google Analytics - add Content Security Policy on header - moved some inline javascript call to a external file

--- track/static/js/https/organizations.js
+++ track/static/js/https/organizations.js
@@ -86,7 +86,7 @@ $(document).ready(function () {
 
     var link = function(link_text) {
       return "" +
-        "<a onClick=\"gtag('event', 'Search', { event_category: 'Search / Rechercher', event_action: 'Search / Rechercher " + row["name_" + language] + "'});\" href=\"/" + language + "/" + text.domains[language] + "/#" +
+        "<a href=\"/" + language + "/" + text.domains[language] + "/#" +
           QueryString.stringify({q: row["name_" + language]}) + "\">" +
            link_text +
         "</a>";
